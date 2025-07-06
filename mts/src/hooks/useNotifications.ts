c539106b348d705137f1cdc64602accdc53afc72
FTS-max <kokoaung332.akz@gmail.com>
2025-07-06 14:05:40 +0630

    deployment2 error fix

--- mts/src/hooks/useNotifications.ts
+++ mts/src/hooks/useNotifications.ts
@@ -1,5 +1,5 @@
 import { useState, useEffect, useCallback } from 'react';
-import { useUser } from '@auth0/nextjs-auth0/client';
+import { useUser } from '@auth0/nextjs-auth0';
 
 export interface Notification {
   id: string;
